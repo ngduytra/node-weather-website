7061fc340f0ad0c79f5f6e3920748c097509e304
Traitimhoada <ngduytra@gmail.com>
2020-04-23 15:58:10 +0700

    remove console.log in js folder

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log('client side javascript is loaded1!')
-
-   
-
 const address = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
